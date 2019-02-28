fcf5350a1e83e6db6c4f4d238e0cd5b5bb9dc402
Ilya <ilyayakimets@gmail.com>
2019-02-28 15:18:35 +0300

    add: 50% of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brs = '[][]()(())([)]'; 
+  let opening = {
+        '[': ']',
+        '{': '}',
+        '(': ')',
+        '<': '>',
+      };
+  
+      let stack = [];
+      let ret = '';
+      let b = true;
+  
+      for(let i = 0; i < brs.length; i++){
+        let c = brs[i];
+  
+        if(opening[c]){
+          stack.push(c);
+        } else {
+          if(stack.length === 0){
+              b = false;
+          }
+  
+          let br = stack.pop();
+  
+          c = opening[br];
+        }
+  
+        ret += c;
+      }
+      if (stack.length > 0){
+        b = false;
+      }
+      return b;
+    }
